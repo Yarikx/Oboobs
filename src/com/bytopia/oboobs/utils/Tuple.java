d94b62ae246e052ad36d8b4063049e5623b3ccab
Yarix <Yarikx@gmail.com>
2012-07-26 17:57:10 +0300

    some tuple fixes

--- src/com/bytopia/oboobs/utils/Tuple.java
+++ src/com/bytopia/oboobs/utils/Tuple.java
@@ -3,10 +3,10 @@ package com.bytopia.oboobs.utils;
 import java.io.Serializable;
 
 public class Tuple <A extends Serializable, B extends Serializable> implements Serializable{
+	private static final long serialVersionUID = -6634496888521867981L;
 	public A a;
 	public B b;
 	public Tuple(A a, B b) {
-		super();
 		this.a = a;
 		this.b = b;
 	}
